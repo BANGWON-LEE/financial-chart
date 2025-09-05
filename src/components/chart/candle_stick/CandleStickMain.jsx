38ef854f62d524a30f23b3fe18d94ef5737150a7
BANGWON LEE <qkddnjs31@gmail.com>
2025-09-05 20:06:55 +0900

    edit : ref 추가

--- src/components/chart/candle_stick/CandleStickMain.jsx
+++ src/components/chart/candle_stick/CandleStickMain.jsx
@@ -2,11 +2,12 @@ import { Chart } from 'react-chartjs-2'
 import 'chartjs-adapter-date-fns'
 
 export default function CandleStickMain(props) {
-  const { data, options, width, height } = props
+  const { data, options, width, height, chartRef } = props
 
   return (
     <div style={{ height: height || 400 }}>
       <Chart
+        ref={chartRef}
         key={Date.now()}
         type="candlestick"
         data={data}
