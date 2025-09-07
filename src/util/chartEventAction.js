ae86554cc516df5270f01e31bf46eac5a4ab9b2b
BANGWON LEE <qkddnjs31@gmail.com>
2025-09-07 21:03:55 +0900

    edit : 콘솔 제거 및 state 조건에 다른 작업 변경

--- src/util/chartEventAction.js
+++ src/util/chartEventAction.js
@@ -1,27 +1,25 @@
 import { setOptionCandleStickData } from '../components/chart/candle_stick/candleStickData'
+import { outerChartRealSignal } from './signal'
 import { xaxisStore } from './xaxisState'
 // import { chartRef } from '../components/chart/candle_stick/CandleStickMain'
 
 const chart = setOptionCandleStickData()
 const xaxis = xaxisStore()
+const signal = outerChartRealSignal()
 
 let xActive = 0
 export const xRangeEvent = (chartRef, setXState) => {
   chartRef.addEventListener('wheel', e => {
-    console.log('wheel event', e)
     if (Math.abs(e.deltaX) > Math.abs(e.deltaY)) {
       // 좌우 휠 동작 감지
       e.preventDefault()
       if (e.deltaX < xActive) {
         xaxis.leftX()
-        console.log('xaxis@@gg', xaxis.getX())
       } else if (e.deltaX > xActive) {
         xaxis.rightX()
-        // setXState(xais)
       }
       setXState(xaxis.getX())
-      // xActive = e.deltaX
-      // chart.pan({ x: xaxis.getX() }, undefined, 'default')
+      if (xaxis.getX() >= -30) signal.update('ChartEvent', true)
     }
   })
 }
