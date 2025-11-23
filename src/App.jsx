45179eb075f6eb6a82dc820a813acb18d17a1c18
BANGWON LEE <qkddnjs31@gmail.com>
2025-11-23 21:41:42 +0900

    add : 서스펜스 예약

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 import Main from './pages/main'
 
@@ -9,7 +10,9 @@ function App() {
 
   return (
     <>
-      <Main />
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <Main />
+      </React.Suspense>
     </>
   )
 }
